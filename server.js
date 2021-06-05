6aadb12c561c54ff875f06d1049f21551d7c23ad
San Nguyen Ngoc <san.nguyenngoc2@duluxgroup.com.au>
2021-06-05 09:39:49 +0700

    Set up react typescript application and custom server

---
+++ server.js
@@ -0,0 +1,28 @@
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({
+    dev,
+    dir: './client',
+    quiet: false
+});
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+    createServer((req, res) => {
+        // Be sure to pass `true` as the second argument to `url.parse`.
+        // This tells it to parse the query portion of the URL.
+        const parsedUrl = parse(req.url, true);
+        const { pathname, query } = parsedUrl;
+
+        handle(req, res, parsedUrl).catch(error => {
+            console.log(error);
+            process.exit(1);
+        });
+    }).listen(3000, (err) => {
+        if (err) throw err
+        console.log('> Ready on http://localhost:3000')
+    })
+});
